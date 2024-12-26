168ed98dd2fdee5e9a2f320abb20fcaf98e2ff13
miguelromero717 <miguelromero717@gmail.com>
2024-12-26 18:20:17 -0500

    chore: Initial commit

---
+++ src/main/java/com/miguelromero717/artistapi/ArtistApiApplication.java
@@ -0,0 +1,13 @@
+package com.miguelromero717.artistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtistApiApplication.class, args);
+    }
+
+}
